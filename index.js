187828267f72989c9f7595a4f9605075ad0affde
unknown <coolshiv0721@gmail.com>
2022-10-19 18:50:49 +0530

    Changed response message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hi there');
+  res.send('Goodbye Goodnight 🙂');
 });
 
 app.listen(8080, () => {
